9f33d8d4252a5a57d59061108c9b99f650b63533
Gustavo de Pinho <gustavo.pinhorezende@gmail.com>
2026-09-02 10:38:00 -0300

    alter grafico plotly to streamlit

--- ui/gastos.py
+++ ui/gastos.py
@@ -123,7 +123,7 @@ def render_gastos(df_gastos):
     df_gastos_historicos = totais["gastos_historicos"].copy()
 
     df_gastos_historicos["Mês"] = (
-      df_gastos_historicos["Mês"].dt.strftime("%b %Y")
+      df_gastos_historicos["Mês"].astype(str)
     )
 
     st.header("Total gasto em Despesas por mês")
